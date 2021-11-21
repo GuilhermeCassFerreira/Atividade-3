330bb607b4c921f1b2fb252919baaad1e415fde1
GuilhermeCassFerreira <94665366+GuilhermeCassFerreira@users.noreply.github.com>
2021-11-21 17:20:36 -0300

    Add files via upload

---
+++ venda.py
@@ -0,0 +1,59 @@
+
+class Venda():
+
+    def __init__(self, codigo: int, cliente: Cliente, descricao: str, pacote: PacoteViagem, quantidade: int):
+        self.__codigo = codigo
+        self.__cliente = cliente
+        self.__descricao = descricao
+        self.__pacote = pacote
+        self.__quantidade = quantidade
+
+    @property
+    def codigo(self):
+        return self.__codigo
+
+    @codigo.setter
+    def codigo(self, codigo: int):
+        self.__codigo = codigo
+
+    @property
+    def cliente(self):
+        return self.__cliente
+
+    @cliente.setter
+    def cliente(self, cliente: Cliente):
+        self.__cliente = cliente
+
+    @property
+    def descricao(self):
+        return self.__descricao
+
+    @descricao.setter
+    def descricao(self, descricao: str):
+        self.__descricao = descricao
+
+    @property
+    def pacote(self):
+        return self.__pacote
+
+    @pacote.setter
+    def pacote(self, pacote: PacoteViagem):
+        self.__pacote = pacote
+
+    @property
+    def quantidade(self):
+        return self.__quantidade
+
+    @quantidade.setter
+    def quantidade(self, quantidade: int):
+        self.__quantidade = quantidade
+
+    def preco_total(self):
+        pacote = self.__quantidade
+        precco = custo_unitario
+        return pacote * 3
+
+teste = Venda(123,'Berti','bom','sds',3)
+test = teste.preco_total()
+print(test)
+
